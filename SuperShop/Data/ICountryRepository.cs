2aa21d06cba9f505198834b3458e02ef3dc5ef1f
Diogoepronto <bta.diogo@gmail.com>
2023-07-10 12:22:50 +0100

    Video 31 - Create CityViewModel - Created all the methods necessary for the ICountryRepository and CountryRepository - Created CountriesController - Created view Index for CoutriesController - Created all necessary actions for the CountriesController and their respective views

--- SuperShop/Data/ICountryRepository.cs
+++ SuperShop/Data/ICountryRepository.cs
@@ -1,9 +1,29 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SuperShop.Data.Entities;
+using SuperShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SuperShop.Data
 {
     public interface ICountryRepository : IGenericRepository<Country>
     {
+        // GET COUNTRIES
+        Task<Country> GetCountryAsync(City city);
 
+        IQueryable GetCountriesWithCities();
+
+        Task<Country> GetCountryWithCitiesAsync(int id);
+
+
+        // CRUD CITIES
+        Task AddCityAsync(CityViewModel model);
+
+        Task<City> GetCityAsync(int id);
+
+        Task<int> UpdateCityAsync(City city);
+
+        Task<int> DeleteCityAsync(City city);
     }
 }
